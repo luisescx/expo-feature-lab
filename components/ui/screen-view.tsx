006edeb8ae4d59f504a9b69ae676099f176d4b1f
luisescx <luis.esculapio@live.com>
2025-09-30 19:26:57 -0300

    feat: create ScreenView component

---
+++ components/ui/screen-view.tsx
@@ -0,0 +1,46 @@
+import { useThemeStore } from "@/stores/theme";
+import { type ViewProps } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { tv } from "tailwind-variants";
+
+export type ThemedViewProps = ViewProps & {
+  themedSpacing?: "sm" | "md" | "lg";
+};
+
+const view = tv({
+  base: "flex-1",
+  variants: {
+    theme: {
+      light: "bg-light-background",
+      dark: "bg-dark-background",
+    },
+    spacing: {
+      sm: "p-1",
+      md: "p-2",
+      lg: "p-4",
+    },
+  },
+  defaultVariants: {
+    spacing: "sm",
+    theme: "light",
+  },
+});
+
+export function ScreenView({
+  themedSpacing = "md",
+  className,
+  ...rest
+}: ThemedViewProps) {
+  const theme = useThemeStore((state) => state.theme);
+
+  return (
+    <SafeAreaView
+      className={view({
+        theme: theme,
+        spacing: themedSpacing,
+        class: className,
+      })}
+      {...rest}
+    />
+  );
+}
