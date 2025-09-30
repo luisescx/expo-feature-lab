81b1f5f2bb8b89dbb99b753130cc9ff8f14aa527
luisescx <luis.esculapio@live.com>
2025-09-30 19:25:25 -0300

    feat: add local storage utils functions

---
+++ utils/storage.ts
@@ -0,0 +1,51 @@
+import { ThemeStates } from "@/stores/theme";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export enum LocalStorageEnum {
+  customTheme = "@expoFeatureLab:customTheme",
+}
+
+interface LocalStorageTypes {
+  [LocalStorageEnum.customTheme]: ThemeStates;
+}
+
+export const LocalStorageService = {
+  async get<K extends keyof LocalStorageTypes>(
+    storageKey: K,
+  ): Promise<LocalStorageTypes[K] | null> {
+    try {
+      const data = await AsyncStorage.getItem(storageKey);
+      return data ? (JSON.parse(data) as LocalStorageTypes[K]) : null;
+    } catch (error) {
+      console.error(`Failed to retrieve ${storageKey}:`, error);
+      return null;
+    }
+  },
+
+  async set<K extends keyof LocalStorageTypes>(
+    storageKey: K,
+    value: LocalStorageTypes[K],
+  ): Promise<void> {
+    try {
+      await AsyncStorage.setItem(storageKey, JSON.stringify(value));
+    } catch (error) {
+      console.error(`Failed to save ${storageKey}:`, error);
+    }
+  },
+
+  async remove(storageKey: keyof LocalStorageTypes): Promise<void> {
+    try {
+      await AsyncStorage.removeItem(storageKey);
+    } catch (error) {
+      console.error(`Failed to remove ${storageKey}:`, error);
+    }
+  },
+
+  async clear(): Promise<void> {
+    try {
+      await AsyncStorage.clear();
+    } catch (error) {
+      console.error("Failed to clear AsyncStorage:", error);
+    }
+  },
+};
