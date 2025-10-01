af04859b9b276db372e770b67bdaef6919ccfd04
luisescx <luis.esculapio@live.com>
2025-10-01 17:22:17 -0300

    feat: create input template component

--- components/ui/index.tsx
+++ components/ui/index.tsx
@@ -1,2 +1,4 @@
+export { Button } from "./button";
+export { Input } from "./input";
 export { ScreenView } from "./screen-view";
 export { ThemedText } from "./themed-text";
---
+++ components/ui/input.tsx
@@ -0,0 +1,11 @@
+import { TextInput, View } from "react-native";
+
+const Input = () => {
+  return (
+    <View className="w-full">
+      <TextInput />
+    </View>
+  );
+};
+
+export { Input };
